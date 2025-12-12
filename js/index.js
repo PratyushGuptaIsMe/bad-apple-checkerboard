ce436046c727b2868cd7235abe8bb5eb508d9890
nimv10 <pratyush.gupta102@cfbmail.com>
2025-12-12 23:15:19 +0000

    init

---
+++ js/index.js
@@ -0,0 +1,20 @@
+let RULE = {
+    false: BLACK,
+    true: WHITE
+}
+
+
+let mainState = [
+    [false, false, false, false, false, false, false, false],
+    [false, false, false, false, false, false, false, false],
+    [false, false, false, false, false, false, false, false],
+    [false, false, false, false, false, false, false, false],
+    [false, false, false, false, false, false, false, false],
+    [false, false, false, false, false, false, false, false],
+    [false, false, false, false, false, false, false, false],
+    [false, false, false, false, false, false, false, false],
+]
+
+mainState[0][1] = true;
+
+console.log(mainState);
