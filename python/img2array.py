5aebbe29ba672b155ad2c15fd198d52a6131c97b
PratyushGuptaIsMe <pratyushngupta@gmail.com>
2025-12-12 20:11:46 -0600

    ADded organization and a gitignore

--- python/img2array.py
+++ python/img2array.py
@@ -1,17 +1,40 @@
 from PIL import Image
+
+
 total_frames = 3087 + 1 # offset
 all_frames = []
 
-for i in range(total_frames):
-    i_str = str(i)
-    frame_number = (5-len(i_str)) * "0" + i_str
-    frame = Image.open(f"../8x8-bad-apple/frame_{frame_number}.png")
-    width, height = frame.size
-    current_frame = []
-    res_divider = 60
-    for x in range(int(width / res_divider)):
-        for y in range(int(height / res_divider)):
-            current_frame.append(frame.getpixel((x,y)))
-    all_frames.append(current_frame)
 
+def get_all_frames():
+    try:
+        for i in range(total_frames):
+            i_str = str(i)
+            frame_number = (5-len(i_str)) * "0" + i_str
+            frame = Image.open(f"../8x8-bad-apple/frame_{frame_number}.png")
+            width, height = frame.size
+            current_frame = []
+            res_divider = 60
+            for x in range(int(width / res_divider)):
+                for y in range(int(height / res_divider)):
+                    current_frame.append(frame.getpixel((x,y)))
+            all_frames.append(current_frame)
+    except FileNotFoundError:
+        print("File path not found. Please edit your settings or smth.")
+    except:
+        print("idk what happened man some error I messed up idk")
+
+
+def frames_int_to_bool():
+    for i in range(len(all_frames)):
+        for j in range(len(all_frames[i])):
+            if(all_frames[i][j] == 0):
+                all_frames[i][j] = False
+            elif(all_frames[i][j] == 255):
+                all_frames[i][j] = True
+            else:
+                print("The image is not black and white ONLY.")
+
+
+get_all_frames()
+frames_int_to_bool()
 print(all_frames)
